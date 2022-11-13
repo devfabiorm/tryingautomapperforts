1e5835ca4fdc70866baf90ac23a8819e715c6bee
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2022-11-13 01:13:45 -0300

    created constructor

--- src/anime/models/anime.ts
+++ src/anime/models/anime.ts
@@ -1,8 +1,16 @@
 import { AutoMap } from '@automapper/classes';
 
 export class Anime {
+  constructor(id: number) {
+    this.id = id;
+  }
+
+  @AutoMap()
+  id = 0;
   @AutoMap()
   title: string;
+  @AutoMap()
   aireFrom: Date;
+  @AutoMap()
   airedTo: Date;
 }
